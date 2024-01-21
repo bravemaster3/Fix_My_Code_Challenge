4ac1bd600dfff58261b2fc5ec2b0235d0b9be75d
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-01-21 21:04:16 +0000

    Fixed 1

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-
+"""square class definition"""
 class square():
-
+    """square class definition"""
     width = 0
     height = 0
 
-
     def __init__(self, *args, **kwargs):
+        """instanciation method"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,10 +15,13 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """Perimeter of the square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """method for nice representation of a square"""
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
